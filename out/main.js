9d4932c61af2a7dd1c5b15c62e63095ce2b3d3ed
lvfulong <775331175@qq.com>
2017-05-08 18:55:43 +0800

    bug

--- out/main.js
+++ out/main.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-if (process.argv.length === 2){
+if (process.argv.length === 2) {
   console.log();
   console.log('用法：');
   console.log('   nativetools create_app [OPTIONS]');
@@ -8,13 +8,13 @@ if (process.argv.length === 2){
   console.log('   create_app');
   console.log('       创建一个runtime项目。');
   console.log('       具体帮助信息用 nativetools create_app --help 查看。');
-   console.log('   refresh_app');
+  console.log('   refresh_app');
   console.log('       刷新当前目录对应的项目的资源和配置。');
   console.log('       具体帮助信息用 nativetools refresh_app --help 查看。');
 }
-  require('yargs').command(require('./createAppCommand'))
+
+require('yargs')
+  .command(require('./createAppCommand'))
+  .command(require('./refreshAppCommand'))
   .help()
- .argv
- require('yargs').command(require('./refreshAppCommand'))
-  .help()
- .argv
+  .argv
