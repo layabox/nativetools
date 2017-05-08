687b8cc8f5c630962694263d60c18c17b4e079e0
lvfulong <775331175@qq.com>
2017-05-08 17:58:30 +0800

    提示

--- out/main.js
+++ out/main.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
-for (var i = 0 ; i < process.argv.length; i++){
-  console.log(' CCCCCCCCCCCCCCC ' + process.argv[i]);
+if (process.argv.length === 2){
+  console.log();
+  console.log('用法：');
+  console.log('   nativetools create_app [OPTIONS]');
+  console.log('   nativetools refresh_app [OPTIONS]');
+  console.log('描述：');
+  console.log('   create_app');
+  console.log('       创建一个runtime项目。');
+  console.log('       具体帮助信息用 nativetools create_app --help 查看。');
+   console.log('   refresh_app');
+  console.log('       刷新当前目录对应的项目的资源和配置。');
+  console.log('       具体帮助信息用 nativetools refresh_app --help 查看。');
 }
   require('yargs').command(require('./createAppCommand'))
   .help()
