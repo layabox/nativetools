26431ddcd76ea78dc7f381b7555fdf89d1d59f1e
lvfulong <775331175@qq.com>
2017-05-08 17:42:10 +0800

    test

--- out/main.js
+++ out/main.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+for (var i = 0 ; i < process.argv.length; i++){
+  console.log(' CCCCCCCCCCCCCCC ' + process.argv[i]);
+}
   require('yargs').command(require('./createAppCommand'))
   .help()
  .argv
